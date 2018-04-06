87480c825de230a7ec9c7b6ddc36f8923bfe82ef
HarishTati <tatiharish123@gmail.com>
2018-04-06 23:16:18 +0530

    server-setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule,Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PersonalInformationComponent } from './personal-information/personal-information.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    PersonalInformationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+        {
+          path : 'home',
+          component : HomeComponent
+        },
+        {
+          path : 'pi',
+          component : PersonalInformationComponent
+        }
+      ]) 
   ],
   providers: [],
   bootstrap: [AppComponent]
